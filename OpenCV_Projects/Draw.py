ff0841828e08a74cbebb3558b1813b6f1094a664
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-12-20 20:21:09 +0530

    Modification

--- OpenCV_Projects/Draw.py
+++ OpenCV_Projects/Draw.py
@@ -1,11 +1,9 @@
-#program for creation of blank image
+#program for creation of blank image in black colour
 import cv2 as cv
 import numpy as np
 
 #create a blank image
 blank=np.zeros((500,500,3), dtype='uint8')
 cv.imshow('Blank',blank)
-
-blank[:]=0,255,0
 
 cv.waitKey(0)
